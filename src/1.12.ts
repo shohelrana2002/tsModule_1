f11453655971b115b4adce99bb291543e8c8b0f4
shohelrana2002 <rana2021tang@gmail.com>
2024-11-01 21:39:54 +0600

    unknown typeof add

--- src/1.12.ts
+++ src/1.12.ts
@@ -8,4 +8,20 @@
     }
   };
   searchName(null);
+
+  //unknown typeof
+
+  const getSpeedMeterPerSecond = (value: unknown) => {
+    if (typeof value === "number") {
+      const convertedSpeed = (value * 1000) / 3600;
+      console.log(convertedSpeed);
+    } else if (typeof value === "string") {
+      const [result, text] = value.split(" ");
+      const convertedSpeed = (parseFloat(result) * 1000) / 3600;
+      console.log(convertedSpeed);
+    } else {
+      console.log("Wrong Input");
+    }
+  };
+  getSpeedMeterPerSecond("dhfdgshgds");
 }
